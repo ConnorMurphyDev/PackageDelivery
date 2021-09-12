21ff52d7d397f65c963a6459d776631714c4ddc1
ConnorMurphyCode <connormurphy94@gmail.com>
2021-09-12 12:42:46 -0400

    Sources update
    Updates text on sources provided

--- truck.py
+++ truck.py
@@ -132,7 +132,7 @@ def decideRoute(truck, distanceMap, hashMap):
     truck.mileage = truck.mileage + newDistance
 
 """
-While no code was quoted, or paraphrased. The Ideas and insperation for this segment was sourced from:
+While no code was quoted, or paraphrased. The solution for the delivery truck pathing (Lines 103-124) was inspired by these sources:
 
 Lawrence, Weru. (2021) Stem Lounge Algorithms for the Traveling Salesman Problem
 https://stemlounge.com/animated-algorithms-for-the-traveling-salesman-problem/
